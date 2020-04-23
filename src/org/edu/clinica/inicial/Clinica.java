710f2d1ca626dcd1c24a8ed194806d61941025f9
emerson <emerson@alves>
2020-04-22 23:28:41 -0300

    alterações iniciais

--- src/org/edu/clinica/inicial/Clinica.java
+++ src/org/edu/clinica/inicial/Clinica.java
@@ -1,10 +1,23 @@
 package org.edu.clinica.inicial;
 
+import org.edu.clinica.clases.Pessoa;
+import org.edu.clinica.util.HibernateUtil;
+import org.hibernate.Session;
+
 public class Clinica {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Pessoa p = new Pessoa();
+		
+		p.setNome("Amanda");
+		
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		session.save(p);
+		session.getTransaction().commit();
+		HibernateUtil.shutdown();
+		
 	}
 
 }
