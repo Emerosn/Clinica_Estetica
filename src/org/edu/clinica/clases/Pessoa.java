869f92992e0310730866c8c22f99f555ae48d423
emerson <emerson@alves>
2020-04-22 23:28:58 -0300

    upload all files

---
+++ src/org/edu/clinica/clases/Pessoa.java
@@ -0,0 +1,27 @@
+package org.edu.clinica.clases;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+
+@Entity
+@Inheritance(strategy = javax.persistence.InheritanceType.JOINED)
+public class Pessoa {
+@GeneratedValue
+@Id
+	private Long id;
+	private String nome;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
